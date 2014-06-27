9e168408f3765dc0ed5d4b73d7f5c5daf68cc593
Regan <reg@cakefarm.com>
2014-06-27 11:20:54 -0700

    Change deploy script.

--- src/dssweb.theme.magic/deploy.php
+++ src/dssweb.theme.magic/deploy.php
@@ -1,44 +1,7 @@
 <?php
-/**
-* GIT DEPLOYMENT SCRIPT
-*
-* Used for automatically deploying websites via github or bitbucket, more deets here:
-*
-* https://gist.github.com/1809044
-*/
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
  
-// The commands
-$commands = array(
-'echo $PWD',
-'whoami',
-'git pull',
-'git status',
-'git submodule sync',
-'git submodule update',
-'git submodule status',
-);
- 
-// Run the commands for output
-$output = '';
-foreach($commands AS $command){
-// Run it
-$tmp = shell_exec($command);
-// Output
-$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-$output .= htmlentities(trim($tmp)) . "\n";
+if ( $_POST['payload'] ) {
+shell_exec( 'cd /var/www/vhosts/cakefarm.com/subdomains/ucd/httpdocs/dss/wireframe-git/dss-plone-sandbox && git reset --hard HEAD && git pull' );
 }
- 
-// Make it pretty for manual user access (and why not?)
-?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-<meta charset="UTF-8">
-<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #FFF; color: #000; font-weight: bold; padding: 0 10px;">
-<pre>
-<?php echo $output; ?>
-</pre>
-</body>
-</html>
