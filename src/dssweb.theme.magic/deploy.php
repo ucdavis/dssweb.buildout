0e934a90ab0e30c10f77fdf775aa4094e50b6c33
Regan <reg@cakefarm.com>
2014-06-27 11:21:46 -0700

    Change deploy script. Test.

--- src/dssweb.theme.magic/deploy.php
+++ src/dssweb.theme.magic/deploy.php
@@ -1,6 +1,6 @@
 <?php
 
-// Use in the "Post-Receive URLs" section of your GitHub repo.
+// Use in the "Post-Receive URLs" section of your GitHub repo. Check.
  
 if ( $_POST['payload'] ) {
 shell_exec( 'cd /var/www/vhosts/cakefarm.com/subdomains/ucd/httpdocs/dss/wireframe-git/dss-plone-sandbox && git reset --hard HEAD && git pull' );
