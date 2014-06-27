cff0f33c02e434d2ea768f377b839ec5fba126b5
Regan <reg@cakefarm.com>
2014-06-27 10:33:04 -0700

    Update deploy script for GitHub Webhook.

--- src/dssweb.theme.magic/ucdavis.wireframe/deploy.php
+++ src/dssweb.theme.magic/ucdavis.wireframe/deploy.php
@@ -36,14 +36,8 @@ $output .= htmlentities(trim($tmp)) . "\n";
 <meta charset="UTF-8">
 <title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #FFF; color: #000; font-weight: bold; padding: 0 10px;">
 <pre>
-. ____ . ____________________________
-|/ \| | |
-[| <span style="color: #FF0000;">&hearts; &hearts;</span> |] | Git Deployment Script v0.1 |
-|___==___| / &copy; oodavid 2012 |
-|____________________________|
- 
 <?php echo $output; ?>
 </pre>
 </body>
