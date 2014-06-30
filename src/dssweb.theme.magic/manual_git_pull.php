7bc99d971a793c890fcb2e3b5cceb417784d000f
Regan <reg@cakefarm.com>
2014-06-30 16:39:06 -0700

    Add manual pull script 15.

--- src/dssweb.theme.magic/manual_git_pull.php
+++ src/dssweb.theme.magic/manual_git_pull.php
@@ -30,7 +30,7 @@ $BRANCH             = "master";
     // If there is already a repo, just run a git pull to grab the latest changes
     // /usr/local/libexec/git-core/git pull
     
-    $res = exec("cd {$LOCAL_REPO} && /usr/local/libexec/git-core/git remote show origin");
+    $res = exec("cd {$LOCAL_REPO} && /usr/local/libexec/git-core/git pull origin master");
 
     echo "pull done res: $res, time: " . mktime();
     
