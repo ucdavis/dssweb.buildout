785e7442e50db04dc169e57e2404a61911d92598
Carol McMasters-Stone <cbeck@ucdavis.edu>
2014-08-20 15:17:25 -0700

    rest of files

---
+++ src/Products.IssEvent/Products/IssEvent/config.py
@@ -0,0 +1,9 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'Products.IssEvent'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'IssEvent': 'Products.IssEvent: Add Iss Event',
+}
---
+++ src/Products.IssEvent/Products/IssEvent/interfaces/__init__.py
@@ -0,0 +1,2 @@
+# -*- extra stuff goes here -*-
+from issevent import IIssEvent
