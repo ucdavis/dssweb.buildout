14ae6de2f3fe8d8618476b783db2ef4714fc1361
Regan <reg@cakefarm.com>
2014-09-02 16:09:55 -0700

    Ad-hoc and index template updates.

---
+++ src/dss.web/dss/web/theme/dss.web-psych/js/jquery.cf.main.js
@@ -0,0 +1,20 @@
+$(function(){	
+	$(document).ready(function() {
+		
+		$(document).foundation(); // foundation top-bar support
+		
+		if (typeof window.console == "object") window.console.log("JQuery version:  " + $.fn.jquery);
+		
+		if($('body.home #top-panel-row').length != 0) {
+			$('body.home #top-panel-row').happybox(
+				{
+					'type': '.panel', // element type to make happy
+					'action_element_class': '.action-element',
+					'canvas_element_class': '.narrow-col',
+					'button_class': ".btn-primary",
+					'height': 520 // total height of the happy viewport
+				}
+			);
+		}
+	});
+});
