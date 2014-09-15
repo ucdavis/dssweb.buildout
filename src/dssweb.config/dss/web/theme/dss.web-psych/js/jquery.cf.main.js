575500ded5c45e1443944dbc9233f2c6bb16b413
Regan <reg@cakefarm.com>
2014-09-15 11:20:20 -0700

    Update to current status. Major changes include T4 completion, navigation and footer refinements for mobile and tablet, and some reorg of styles.scss.

--- src/dssweb.config/dss/web/theme/dss.web-psych/js/jquery.cf.main.js
+++ src/dssweb.config/dss/web/theme/dss.web-psych/js/jquery.cf.main.js
@@ -6,30 +6,23 @@
 	var currentMQ = "unknown"; // set the default screen size
 	var front = false;
 	var front_banner_height = 520;
-
+	var vcardiced = false;
+	
 	$(document).ready(function() {
-		if (typeof window.console === "object") {
-			window.console.log("JQuery version:  " + $.fn.jquery);
+		
+		// JQuery version
+		if(debug) {
+			if(typeof window.console !== "undefined") {
+				window.console.log("JQuery version:  " + $.fn.jquery);
+			}
 		}
 		
-		// foundation top-bar support. Note: Add after any JavaScript constructed markup that relies on foundation
+		// foundation top-bar support. 
+		// Note: Add after any JavaScript constructed markup that relies on foundation
 		$(document).foundation(); 
-				
-		// Activate home page happy boxes
-		if($('body.home #top-panel-row').length !== 0) {
-			$('body.home #top-panel-row').happybox(
-				{
-					'type': '.panel', // element type to make happy
-					'action_element_class': '.action-element',
-					'canvas_element_class': '.narrow-col',
-					'button_class': ".btn-primary",
-					'height': front_banner_height // total height of the happy viewport
-				}
-			);
-			front = true;
-		}
 		
-		// Get picture figure/figcaption
+		// PLUGIN CANDIDATES /////////////////////////////////////////////
+		// Process .picture into figure/figcaption
 		// TODO: Plugin
 		if($('.picture').length !== 0) {
       $('.picture img').each(function () {
@@ -39,28 +32,61 @@
       });
 		}		
 		
+		// Process download file icons
+		// TODO: Plugin ... with option to add id's which should be checked for file type icon placement
+		if($('#downloads').length !== 0) {
+      $('#downloads li').each(function () {
+				// Method using href
+				var a = $(this).find('a');
+				var c = a.attr('href').split(".");
+				if( c.length === 1 || ( c[0] === "" && c.length === 2 ) ) {
+				    // handle errors
+				} else {
+					a.addClass(c.pop()+'-icon icon-processed');
+				}
+				// Method using filetype class in cases where PDF or other extension not available
+				/*if($(this).has('span.pdf-filetype')) {
+					$(this).find('a').addClass('pdf-icon icon-processed');
+				}*/
+      });
+		}		
+		
     // Add Events Cross-browser
     var event = {
-        add: function (elem, type, fn) {
-            if (elem.attachEvent) {
-                elem['e' + type + fn] = fn;
-                elem[type + fn] = function () {
-                    elem['e' + type + fn](window.event);
-                };
-                elem.attachEvent('on' + type, elem[type + fn]);
-            } else {
-                elem.addEventListener(type, fn, false);
-						}
-        }
+	    add: function (elem, type, fn) {
+        if (elem.attachEvent) {
+	        elem['e' + type + fn] = fn;
+	        elem[type + fn] = function () {
+	        	elem['e' + type + fn](window.event);
+	        };
+	        elem.attachEvent('on' + type, elem[type + fn]);
+        } else {
+        	elem.addEventListener(type, fn, false);
+				}
+	    }
     };
 
-    // Set default
+		
+		// WAI level-1 compliancy - Bypass Blocks 2.4.1
+		// Nic Zakas "skip to content" next focus fix
+		// See http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+		// TEST Checkpoint: https://code.google.com/p/chromium/issues/detail?id=37721
+		window.addEventListener("hashchange", function(event) {
+	    var element = document.getElementById(location.hash.substring(1));
+	    if (element) {
+	      if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+	      	element.tabIndex = -1;
+	      }
+	      element.focus();
+	    }
+		}, false);
    
 
     // Check CSS value in active media query and sync Javascript functionality
     var mqSync = function () {
         // Fix for Opera issue when using font-family to store value
         if (window.opera) {
+					// TODO: Check that content-main is available after changing this from class to id
            activeMQ = window.getComputedStyle(document.body, ':after').getPropertyValue('content-main');
         }
         // For all other modern browsers
@@ -71,6 +97,7 @@
         else {
             // Use .getCompStyle instead of .getComputedStyle so above check for window.getComputedStyle never fires true for old browsers
             window.getCompStyle = function (el, pseudo) {
+							this.pseudo = pseudo;
                 this.el = el;
                 this.getPropertyValue = function (prop) {
                     var re = /(\-([a-z]){1})/g;
@@ -93,6 +120,20 @@
         activeMQ = activeMQ.replace(/'/g, "");
     }; // End mqSync
 		
+		var bend = function () {
+			if(($('#footer #vcard').length !== 0) && !vcardiced) {
+				$('#footer #vcard').insertBefore($('#footer #copyright'));
+				vcardiced = true;
+			}
+		};
+		
+		var unbend = function () {
+			if(vcardiced) {
+				$('#footer #vcard').insertAfter($('#footer #foot-social'));
+				vcardiced = false;
+			}
+		};
+		
 		var expand = function () {
 
       // Conditions for each breakpoint
@@ -103,6 +144,8 @@
               currentMQ = activeMQ;
 							utilStack();
               collapsedView();
+							
+							bend();
 
           }
 					// If the detected screen size is Mobile 450px-599px
@@ -113,6 +156,7 @@
 						 
 							collapsedView();					
 							
+							bend();
 							
           }
 					// If the detected screen size is Tablet 600px-959px
@@ -129,6 +173,7 @@
 							
 							}
 							*/
+							bend();
           }
 					// If the detected screen size is Desktop 960px and up
           if (activeMQ === 'L') {
@@ -136,6 +181,8 @@
 							rmvStack();
 							$('body.home #top-panel-row').height(front_banner_height);
               expandedView();
+							
+							unbend();
           }
       }
 		};
@@ -151,10 +198,28 @@
         event.add(window, "resize", myResize);
     }
 
+		// Activate home page happy boxes
+		if ((activeMQ === 'L') || (activeMQ === 'M')) {
+			if($('body.home #top-panel-row').length !== 0) {
+				$('body.home #top-panel-row').happybox(
+					{
+						'type': '.panel', // element type to make happy
+						'action_element_class': '.action-element',
+						'canvas_element_class': '.narrow-col',
+						'button_class': ".btn-primary",
+						'height': front_banner_height // total height of the happy viewport
+					}
+				);
+				front = true;
+			}
+		}
+		
     if (debug) {
         $(window).resize(function () {
-            console.log($(this).width());
-						console.log('expand.js detected screen size: ' + currentMQ);
+					if(typeof window.console !== "undefined") {
+            window.console.log($(this).width());
+						window.console.log('Screen size: ' + currentMQ);
+					}
         });
     }
 		
@@ -195,8 +260,8 @@
 				// Fix for contA height not being reset to auto	
 				$(".collapsible").css("height", "auto");
 				
-				$(".content-main div").children().removeClass("panel-collapse collapse active");
-				$(".content-main .panel-heading").hide();
+				$("#content-main div").children().removeClass("panel-collapse collapse active");
+				$("#content-main .panel-heading").hide();
 				$(".panel-group").children().removeClass("panel panel-default");
         $(".more_text").hide();
 				
