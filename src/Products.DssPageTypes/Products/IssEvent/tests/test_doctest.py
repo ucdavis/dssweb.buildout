aaa51a336886de9e614c53da66e1713ef0be07db
Carol McMasters-Stone <cbeck@ucdavis.edu>
2014-11-18 17:13:16 -0800

    added normalization for locations, rooms and buildings in support of wayfinding

--- src/Products.DssPageTypes/Products/IssEvent/tests/test_doctest.py
+++ src/Products.DssPageTypes/Products/IssEvent/tests/test_doctest.py
@@ -21,5 +21,5 @@ def test_suite():
 
         ])
 
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
+#if __name__ == '__main__':
+#    unittest.main(defaultTest='test_suite')
