bb3ed7d9a4e8e00e1ad967ec2ce6c18a43f56574
Steve McMahon <steve@dcn.org>
2015-01-29 08:59:50 -0800

    slightly optimize view

--- src/dssweb.facnavviews/dssweb/facnavviews/facnav_support.py
+++ src/dssweb.facnavviews/dssweb/facnavviews/facnav_support.py
@@ -5,16 +5,19 @@ from zope.component import getMultiAdapter
 class FacNavView(BrowserView):
     """ support for customized fac nav views """
 
+    def __init__(self, context, request):
+        super(FacNavView, self).__init__(context, request)
+        portal_state = getMultiAdapter((self.context, self.request),
+            name=u"plone_portal_state")
+        self.portal_url = portal_state.portal_url()
+        self.nav_url = portal_state.navigation_root_url()
+
     def navRootRelativeUrl(self, obj):
         """
             Return a URL that has been adjusted to traverse
             to the object by acquisition via the nav root.
         """
 
-        portal_state = getMultiAdapter((self.context, self.request),
-            name=u"plone_portal_state")
-        portal_url = portal_state.portal_url()
-        nav_url = portal_state.navigation_root_url()
         obj_url = obj.absolute_url()
 
-        return obj_url.replace(portal_url, nav_url)
+        return obj_url.replace(self.portal_url, self.nav_url)
