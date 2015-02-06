2f6a705d88f49ea2070efc8412e30f42e5f8fdf3
Carol McMasters-Stone <cbeck@ucdavis.edu>
2015-02-06 11:37:13 -0800

    changing to an embedded pfg contact form

--- src/Products.DssPageTypes/Products/DssPageTypes/browser/DssPageViews.py
+++ src/Products.DssPageTypes/Products/DssPageTypes/browser/DssPageViews.py
@@ -1,6 +1,7 @@
 from Products.Five.browser import BrowserView
 from Acquisition import aq_inner
 from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
 
 
 class DssContactView(BrowserView):
@@ -9,6 +10,12 @@ class DssContactView(BrowserView):
     def __init__(self, context, request):
             self.context = context
             self.request = request
+            
+    def render_form(self):
+         portal = getToolByName(self.context, 'portal_url').getPortalObject()
+         form_view = portal.restrictedTraverse('contact/contact-form/@@embedded')
+         form_view.prefix = 'mypfg'
+         return form_view()
             
     def portal_state(self):
          context = aq_inner(self.context)
