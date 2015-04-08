d51e590d48ef8746ecd299ab2f8f5f4aa26cc732
Steve McMahon <steve@dcn.org>
2015-04-08 10:47:32 -0700

    Optimize fetching of department by using facnav criteria

--- src/dssweb.facnavviews/dssweb/facnavviews/facnav_support.py
+++ src/dssweb.facnavviews/dssweb/facnavviews/facnav_support.py
@@ -1,6 +1,12 @@
+from eea.facetednavigation.interfaces import ICriteria
+from plone.memoize import instance
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from zope.component import getMultiAdapter
-from Products.FacultyStaffDirectory import Person
+
+
+def _getObjectByUID_cachekey(method, self, UID):
+    return UID
 
 
 class FacNavView(BrowserView):
@@ -12,6 +18,7 @@ class FacNavView(BrowserView):
             name=u"plone_portal_state")
         self.portal_url = portal_state.portal_url()
         self.nav_url = portal_state.navigation_root_url()
+        self.catalog = getToolByName(context, 'portal_catalog')
 
     def navRootRelativeUrl(self, obj):
         """
@@ -19,6 +26,42 @@ class FacNavView(BrowserView):
             to the object by acquisition via the nav root.
         """
 
+        # import pdb; pdb.set_trace()
+        # dept = obj.getPrimaryDepartment()
+        # mi = dept.getMembershipInformation(obj)
+
         obj_url = obj.absolute_url()
 
         return obj_url.replace(self.portal_url, self.nav_url)
+
+    def defaultForVocabulary(self, vocabulary):
+        """
+            return the faceted nav default for a particular criteria
+        """
+
+        for criterion in ICriteria(self.context).criteria:
+            if criterion.vocabulary == vocabulary:
+                return criterion.default
+
+        return None
+
+    @instance.memoize
+    def getObjectByUID(self, UID):
+        """
+            find the object with this UID
+        """
+
+        brains = self.catalog(UID=UID)
+        return brains[0].getObject()
+
+    def getFacNavDepartment(self):
+        """
+            Get the FSD department criteria for this
+            FacetedNavigation views
+        """
+
+        dept_uid = self.defaultForVocabulary(u'Department Names')
+        if dept_uid:
+            return self.getObjectByUID(dept_uid)
+
+        return None
