eef12171349ca330bf0d2e059f85df1c96fc6e9b
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-07-18 12:46:40 -0700

    ex method to migrated 2columnpages

--- src/Products.DssPageTypes/Products/DssPageTypes/config.py
+++ src/Products.DssPageTypes/Products/DssPageTypes/config.py
@@ -6,7 +6,7 @@ PROJECTNAME = 'Products.DssPageTypes'
 ADD_CONTENT_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'DssContact': 'Products.DssPageTypes: Add Dss Contact',
-    'DssTwoColumnPage': 'Products.DssPagetypes: Add Two column Page',
-    'DssTwoCollumnPage': 'Products.DssPagetypes: Add Two Column Page',
+    'DssTwoColumnPage': 'Products.DssPagetypes: Add Two Column Page',
+    'DssTwoCollumnPage': 'Products.DssPagetypes: Add Two column Page',
     'DssCourse': 'Products.DssPageTypes: Add Dss Course',
 }
