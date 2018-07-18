e8e803c6663391368149d9eb12804e6581890153
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-07-18 13:58:48 -0700

    remove permissions for incorrect type

--- src/Products.DssPageTypes/Products/DssPageTypes/config.py
+++ src/Products.DssPageTypes/Products/DssPageTypes/config.py
@@ -7,6 +7,5 @@ ADD_CONTENT_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'DssContact': 'Products.DssPageTypes: Add Dss Contact',
     'DssTwoColumnPage': 'Products.DssPagetypes: Add Two Column Page',
-    'DssTwoCollumnPage': 'Products.DssPagetypes: Add Two column Page',
     'DssCourse': 'Products.DssPageTypes: Add Dss Course',
 }
