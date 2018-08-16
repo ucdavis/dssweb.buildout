c050ce7625058bbcbed31955069c5922092d8aa8
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-08-16 11:01:54 -0700

    fixing permission typos

--- src/Products.DssPageTypes/Products/DssPageTypes/config.py
+++ src/Products.DssPageTypes/Products/DssPageTypes/config.py
@@ -6,7 +6,7 @@ PROJECTNAME = 'Products.DssPageTypes'
 ADD_CONTENT_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'DssContact': 'Products.DssPageTypes: Add Dss Contact',
-    'DssTwoColumnPage': 'Products.DssPagetypes: Add Two Column Page',
+    'DssTwoColumnPage': 'Products.DssPageTypes: Add Two Column Page',
     'DssCourse': 'Products.DssPageTypes: Add Dss Course',
     'DssTwoCollumnPage': 'Products.DssPageTypes: Add Two Collumn Page',
 }
